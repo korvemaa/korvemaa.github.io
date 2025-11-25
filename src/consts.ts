c40d479a62dc94e23f68e1f317b55c3121e220b6
Jake Korvemaa <55055481+Jake-NSW@users.noreply.github.com>
2025-11-25 23:27:26 +1100

    Correct head data

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jakes Place';
-export const SITE_DESCRIPTION = '';
+export const SITE_TITLE = 'Jakes Korvemaa';
+export const SITE_DESCRIPTION = 'Jakes personal website';
 
 export const THEME_HUE_KEY = 'terminal-hue';
