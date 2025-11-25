2ae3bcac5a8e492101c37c561510e0cb10f8c692
Jake Korvemaa <55055481+Jake-NSW@users.noreply.github.com>
2025-11-25 23:36:17 +1100

    add site path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://korvemaa.github.io',
+	trailingSlash: 'never',
 	integrations: [mdx(), sitemap()],
+	output: 'static'
 });
